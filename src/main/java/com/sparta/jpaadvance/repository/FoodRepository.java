1a91120e2c124627cabf981fb366d82825f45ed9
Sohyun <sh-119@naver.com>
2025-02-05 23:13:29 +0900

    @OneToOne 단방향 테스트

---
+++ src/main/java/com/sparta/jpaadvance/repository/FoodRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.jpaadvance.repository;
+
+import com.sparta.jpaadvance.entity.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food,Long> {
+
+
+
+}
